e58a8ca660451bc92f0aa4721096797102b1c87f
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-02-01 15:13:08 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'glicogabi-v35';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icone.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
