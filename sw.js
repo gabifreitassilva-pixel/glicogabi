b1894863f9f5efe7355c17d147cc4dc0e1455b85
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-02-02 21:09:44 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('App Cozinha da Gabi instalado com sucesso!');
-});
-
-self.addEventListener('fetch', (event) => {
-    // Mantém o app funcionando online e pronto para cache futuro
-    event.respondWith(fetch(event.request));
-});
